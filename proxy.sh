99d03983a686c3fd2f1a2e8266807311982c5ae1
Hu.Nan <nhu@linx-info.com>
2021-03-09 10:52:40 +0800

    proxy

---
+++ proxy.sh
@@ -0,0 +1,5 @@
+export http_proxy="http://127.0.0.1:8889"
+export https_proxy="https://127.0.0.1:8889"
+#export http_proxy="sock5://127.0.0.1:8889"
+#export https_proxy="sock5//127.0.0.1:8889"
+#export ALL_PROXY="sock5//127.0.0.1:8889"
